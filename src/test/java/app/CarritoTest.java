d50a7d0f8b1ca242e2d3332bd079c182870fb80f
Miguel Ramos <return.infox@gmail.com>
2023-06-08 19:51:44 -0400

    carrito test completada

--- src/test/java/app/CarritoTest.java
+++ src/test/java/app/CarritoTest.java
@@ -1,6 +1,9 @@
 package app;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class CarritoTest {
 
@@ -17,6 +20,47 @@ public class CarritoTest {
     producto2 = new Producto("Mouse Razer", 65000);
     carrito.agregarProducto(producto1);
   }
+  
+  @Test
+  public void testAgregarProducto() {
+    // assertEquals(expect, currentValue)
+    assertEquals(1, carrito.getProductos().size());
+    
+    // Agregar un segundo producto
+    carrito.agregarProducto(producto2);
+    assertEquals(2, carrito.getProductos().size());
+  }
+  
+  @Test
+  public void testEliminarProducto() {
+    carrito.eliminarProducto(producto1);
+    // Verificar que la cantidad de productos sea la correcta (0), despues de eliminar un producto
+    assertEquals(0, carrito.getProductos().size());
+  }
+  
+  
+  @Test
+  public void testCalcularTotal() {
+    double total = carrito.calcularTotal();
+    assertEquals(250000,total,0.0);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
 }
